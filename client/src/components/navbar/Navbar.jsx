d86b8b95d8b2bad111be447c2a26721c4c1b57ab
Dev-scott <sadoscott25@gmail.com>
2023-05-16 07:51:54 +0200

    work

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -39,12 +39,12 @@ function Navbar() {
       <div className="container">
         <div className="logo">
           <Link className="link" to="/">
-            <span className="text">fiverr</span>
+            <span className="text"><img src="/img/logo.png" alt="" className="logo" /></span>
           </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Fiverr Business</span>
+          <span className="link_explore">Explore</span>
           <span>Explore</span>
           <span>English</span>
           {!currentUser?.isSeller && <span>Become a Seller</span>}
